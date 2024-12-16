e53990d122391e34bf11e9d20ccd3a255e9dfa9f
Victoria Adebobola <117773755+Adeyanfe@users.noreply.github.com>
2024-12-16 14:46:36 +0100

    Update Diabetes Prevention Program Analysis Using.sql

--- Diabetes Prevention Program Analysis Using.sql
+++ Diabetes Prevention Program Analysis Using.sql
@@ -5,13 +5,12 @@
 ```sql
 SELECT *
 FROM DiabetesPreventionProgram;
-```
+
 
 ---2. Checking for Null Values
-```sql
 SELECT COUNT(*) AS missing_data_count
 FROM DiabetesPreventionProgram
 WHERE location_1 IS NULL OR city IS NULL OR telephone_number IS NULL;
-```
+
 
 
